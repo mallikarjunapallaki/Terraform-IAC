7221db11aa2021a269561bb0735491598c5f96ba
Mallikarjuna Reddy Pallaki <mpallaki@pkrmsezdc.com>
2026-08-12 16:41:22 +0530

    added azure credentials

--- test-github-actions-iac/variables.tf
+++ test-github-actions-iac/variables.tf
@@ -27,3 +27,8 @@ variable "owner" {
   type        = string
   default     = "Mallikarjuna"
 }
+
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
